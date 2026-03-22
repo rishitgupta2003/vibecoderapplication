e086610f98037625a77c21092d60b47ac2e4a3a6
rishitgupta <rishitgupta203@gmail.com>
2026-03-22 14:19:25 +0530

    Initial Setup Cont..

---
+++ src/main/java/dev/rishit/vibecoder/service/PlanService.java
@@ -0,0 +1,13 @@
+package dev.rishit.vibecoder.service;
+
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PlanService {
+}
