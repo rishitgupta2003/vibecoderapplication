c1998ff13fd3a68c4dee4abcb2da97c2a4cf98aa
Rishit Gupta <69597366+rishitgupta2003@users.noreply.github.com>
2026-07-03 20:11:59 +0530

    some changes

---
+++ src/main/java/dev/rishit/vibecoder/service/chat/ChatModelService.java
@@ -0,0 +1,5 @@
+package dev.rishit.vibecoder.service.chat;
+
+public interface ChatModelService {
+    String generateResponse(String message);
+}
---
+++ src/main/java/dev/rishit/vibecoder/service/impl/chat/DialChatService.java
@@ -0,0 +1,18 @@
+package dev.rishit.vibecoder.service.impl.chat;
+
+import dev.rishit.vibecoder.service.chat.ChatModelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DialChatService implements ChatModelService {
+
+    private final ChatModel chatModel;
+
+    @Override
+    public String generateResponse(String message) {
+        return null;
+    }
+}
